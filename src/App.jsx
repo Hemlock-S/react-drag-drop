9cd5cad126fc2f94ae1bac27e54c87dff5e1d417
Siam Bhuiyan <bhuiyansiam077@gmail.com>
2024-12-08 17:25:52 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import DropFileInput from './components/drop-file-input/DropFileInput';
 
 function App() {
 
-  const onFileChange = (files) => {
-    console.log(files);
-  };
-
   return (
     <div className='box'>
      <h2 className="header">
